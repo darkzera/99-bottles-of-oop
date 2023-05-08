0fde0c5420a50fdff30ee581058ad0e152747765
darkzera <lucas.zrc@gmail.com>
2023-05-08 18:53:09 -0300

    Basic to reach open princpl

--- src/main/java/com/darkzera/bottle99/Bottle.java
+++ src/main/java/com/darkzera/bottle99/Bottle.java
@@ -13,43 +13,40 @@ public class Bottle {
         return result;
     }
 
-    public String verse(int quantity){
-
-        switch (quantity){
-
-            case 1:
-                return quantity + " bottle of beer on the wall, "
-                        + quantity + " bottle of beer.\n" +
-                        "Take it down and pass it around, no more bottles of beer on the wall.\n";
+    public String verse(int number){
+        switch (number){
 
             case 0:
                 return "No more bottles of beer on the wall, " +
                         "no more bottles of beer.\n" +
+
                         "Go to the store and buy some more, " +
                         "99 bottles of beer on the wall.\n";
 
-            case 2:
-                return "2 bottles of beer on the wall, " +
-                        "2 bottles of beer.\n" +
-                        "Take one down and pass it around, " +
-                        "1 bottle of beer on the wall.\n";
+            case 1:
+                return  number + " "+container(number) + " of beer on the wall, " +
+                        number +" "+ container(number) +  " of beer.\n" +
 
-            case 99:
-                return "99 bottles of beer on the wall, " +
-                        "99 bottles of beer.\n" +
-                        "Take one down and pass it around, " +
-                        "98 bottles of beer on the wall.\n";
+                        "Take it down and pass it around, " +
+                        "no more bottles of beer on the wall.\n";
+
             default:
-                return quantity + " bottles of beer on the wall, " + quantity +" bottles of beer.\n" +
-                        "Take one down and pass it around, " +
-                        --quantity + " bottles of beer on the wall.\n";
+                return  number + " " + container(number) + " of beer on the wall, " +
+                        number + " " + container(number) + " of beer.\n" +
 
+                        "Take one down and pass it around, " +
+                        --number + " " + container(--number) + " of beer on the wall.\n";
         }
     }
 
     public String song(){
         return verses(99, 0);
     }
+
+    public String container(int number){
+        return number <= 1 ? "bottle" : "bottles";
+    }
+
 
 
 
